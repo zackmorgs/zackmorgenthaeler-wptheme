f60414e643f9acaa7e1421d54e0fc68f70abcffa
Zack Morgenthaler <zackmorgenthaler@gmail.com>
2024-08-09 17:34:09 -0500

    added more elements of wordpress themes

--- index.php
+++ index.php
@@ -21,8 +21,10 @@
         <?php while (have_posts()):
             the_post(); ?>
             <div class="post">
-                <h2><a href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?></a>
+                <h2>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
                 </h2>
                 <?php the_content(); ?>
             </div>
