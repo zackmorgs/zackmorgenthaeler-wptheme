e43ab4eba7a4ba3c5f2b550de8a60087d92d3302
Zack Morgenthaler <zackmorgenthaler@gmail.com>
2024-08-09 17:55:19 -0500

    updated single.php

--- single.php
+++ single.php
@@ -16,6 +16,20 @@
             <?php bloginfo('description'); ?>
         </p>
     </header>
+    <main>
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <article>
+                    <h2>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </main>
     <?php get_footer(); ?>
 </body>
 </html>
